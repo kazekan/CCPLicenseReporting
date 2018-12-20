d17cccca9af19907ab7602059974979e9e587241
Sean May <sean.may08@gmail.com>
2018-12-20 12:27:28 -0600

    Initial commit

---
+++ CCP Endpoint Reporting v2.ps1
@@ -0,0 +1,110 @@
+﻿#-----------------------------------|
+#	Written by Sean May - CyberArk	|
+#	December 6, 2018				|
+#	Version 1						|
+#	Note: This is not an official	|
+#	CyberArk utility.				|
+#-----------------------------------|
+
+# Usage: This script assumes that it will be ran on a CCP server that is NOT also being used
+# as a standalone Credential Provider. Because the script cannot differentiate between CCP calls
+# and direct CP calls, it will be very important to ensure it's only ran on CCP servers for
+# accurate reporting. Once the CSV file is generated, the user will need to manually combine
+# that file with reports from other CCP servers in order to provide a full list of the endpoints
+# and remove any duplicates between them, at least for now. ;)
+
+$installDir = "unknown"
+
+function Find-SavePath {
+    [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+    [System.Windows.Forms.Application]::EnableVisualStyles()
+    $browse = New-Object System.Windows.Forms.FolderBrowserDialog
+    $browse.SelectedPath = "C:\"
+    $browse.ShowNewFolderButton = $true
+    $browse.Description = "Select a directory where the report will be placed"
+
+    $loop = $true
+    while($loop)
+    {
+        if ($browse.ShowDialog() -eq "OK")
+        {
+        $loop = $false
+				
+        } else
+        {
+            $res = [System.Windows.Forms.MessageBox]::Show("You clicked Cancel. Would you like to try again or exit?", "Select a location", [System.Windows.Forms.MessageBoxButtons]::RetryCancel)
+            if($res -eq "Cancel")
+            {
+                #Ends script
+                return
+            }
+        }
+    }
+    $browse.SelectedPath
+    $browse.Dispose()
+}
+
+function Find-InstallDir {
+
+    if (Test-Path "D:\Program Files (x86)\CyberArk\ApplicationPasswordProvider\Logs\") {
+        Set-Variable installDir "D:\Program Files (x86)\CyberArk\ApplicationPasswordProvider\Logs\"
+        } elseif (Test-Path "C:\Program Files (x86)\CyberArk\ApplicationPasswordProvider\Logs\") {
+            Set-Variable installDir "C:\Program Files (x86)\CyberArk\ApplicationPasswordProvider\Logs\"
+        } else {
+                Set-Variable installDir "unknown"
+        }
+    $installDir
+}
+
+function Find-LogPath($installDir) {
+
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
+ Out-Null
+
+ $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+ $OpenFileDialog.initialDirectory = $installDir
+ $OpenFileDialog.ShowDialog() | Out-Null
+ $OpenFileDialog.filename
+
+}
+
+Set-Variable savePath (Find-SavePath)
+$savePath = $savePath + "\CCP.csv"
+
+Set-Variable installDir (Find-InstallDir)
+
+if ($installDir -contains "unknown") {
+    Read-Host "Please hit Enter to choose the location of your AppAudit.log file"
+    Set-Variable logPath (Find-LogPath $installDir)
+    } else {
+        $logPath = $installDir + "\AppAudit.log"
+        }
+
+Write-Host "The report will be saved to: " $savePath
+Write-Host "The log will be pulled from: " $logPath
+
+# Parse the log file
+$addressPattern = '(?<=IP address \[).+?(?=\])'
+
+$data = Get-Content -Path $logPath | Where-Object {$_ -Match '.*APPAU005I.*'}
+$addresses = $data | Select-String $addressPattern -AllMatches
+$uniqueAddresses = $addresses.Matches.Value | select -uniq
+
+# Output
+Write-Host "`n"
+Write-Host "Unique addresses found:`n"
+$uniqueAddresses
+Write-Host "`n"
+Write-Host ("Total count of unique addresses: " + $uniqueAddresses.Count)
+
+foreach($item1 in $uniqueAddresses) 
+{ 
+  $csv_string = "";
+  foreach($item in $item1)
+  {
+    $csv_string = $csv_string + $item;
+  }
+  Set-Content $savePath $csv_string;
+}
+
+Read-Host -Prompt “`nPress Enter to exit”
